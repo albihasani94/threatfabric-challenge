ebce335966b209f9bbd56f023e03c74952b87f9d
albihasani94 <albihasani94@gmail.com>
2020-10-09 15:25:22 +0200

    Add some data population migration scripts

---
+++ repository/src/main/resources/db/migration/V3__populate_devices.sql
@@ -0,0 +1,5 @@
+insert into device (id, model, os_version, type) values ('9f64824c-0a54-4545-9774-b4a94f3e721d', 'iPhone 13', 13.0, 'IOS');
+insert into device (id, model, os_version, type) values ('9f64824c-0a54-4545-9774-b4a94f3e720e', 'iPhone 13', 13.0, 'IOS');
+insert into device (id, model, os_version, type) values ('9f64824c-0a54-4545-9774-b4a94f3e721e', 'iPhone 12', '13.1.2 Hotfix', 'IOS');
+insert into device (id, model, os_version, type) values ('9f64824c-0a54-4545-9774-b4a94f3e722e', 'iPhone 13', 1, 'IOS');
+
---
+++ repository/src/main/resources/db/migration/V5__populate_detections_new.sql
@@ -0,0 +1,17 @@
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (64, '16a9d528-fb60-4ec3-8891-7b99df18f914', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (67, '16a9d528-fb60-4ec3-8891-7b99df18f915', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (70, '16a9d528-fb60-4ec3-8891-7b99df18f916', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (73, '16a9d528-fb60-4ec3-8891-7b99df18f917', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (76, '16a9d528-fb60-4ec3-8891-7b99df18f918', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (77, '16a9d528-fb60-4ec3-8891-7b99df18f919', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (78, '16a9d528-fb60-4ec3-8891-7b99df18f920', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (80, '16a9d528-fb60-4ec3-8891-7b99df18f921', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (81, '16a9d528-fb60-4ec3-8891-7b99df18f923', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (84, '4b91f16b-6097-4ec9-b86f-c4317c6340bd', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (87, 'ad753145-7f93-49c0-a593-119a5f39e477', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (90, '16a9d528-fb60-4ec3-8891-7b99df18f926', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (92, '50c3b302-5688-40c7-98d0-f6c82295144d', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (95, '64ca0ad7-baf0-4b62-b7c7-9c8bcdb468a1', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (101, '64ca0ad7-baf0-4b62-b7c7-9c8bcdb468a2', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (104, '64ca0ad7-baf0-4b62-b7c7-9c8bcdb468a3', '1st on iPhone 13', 'Banking');
+insert into detection_new (id, detection_uuid, name_of_app, type_of_app) values (108, '64ca0ad7-baf0-4b62-b7c7-9c8bcdb468a4', '1st on iPhone 13', 'Banking');
